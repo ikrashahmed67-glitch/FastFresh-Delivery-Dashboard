c7f11be62aacd38a0c1e173a673bc424ba9f8580
ikrashahmed67-glitch <ikrashahmed67@gmail.com>
2025-12-09 18:27:24 +0500

    Initial commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://yaflexlocwmhocjfrpov.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhZmxleGxvY3dtaG9jamZycG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMDY0MTksImV4cCI6MjA4MDc4MjQxOX0.NB_KocoDo8HX3B0hRJuLF-exBdaKmMo--JNbSgqEOXk";
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
